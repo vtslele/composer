d98fc504301ef6ec07777ffaf9cbe62ab3db1beb
wangle <wangle@vchangyi.com>
2022-04-27 16:04:44 +0800

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac126
+ * Date: 2022/4/27
+ * Time: 15:18
+ */
+
+require "vendor/autoload.php";
+
+$hello = new \App\Demo();
+
+$hello->Test();
---
+++ src/Demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac126
+ * Date: 2022/4/27
+ * Time: 15:43
+ */
+
+namespace App;
+
+class Demo
+{
+    public function Test()
+    {
+        echo "Hello, Composer!", PHP_EOL;
+    }
+
+}
